105a09df51855a1bca583807623f1a5687a5baf4
Joel <joelmartinez@veltium.com>
2021-05-10 12:14:18 +0200

    test2

--- VeltiumCharger/main/coms/COAP_Server.cpp
+++ VeltiumCharger/main/coms/COAP_Server.cpp
@@ -12,23 +12,13 @@ extern carac_group  ChargingGroup;
 extern carac_Comands  Comands ;
 
 
-
-// CoAP client response callback
-void callback_response(CoapPacket &packet, IPAddress ip, int port);
-
-// CoAP server endpoint url callback
-void callback_light(CoapPacket &packet, IPAddress ip, int port);
-
 // UDP and CoAP class
 WiFiUDP  Udp;
 Coap coap(Udp);
 
-// LED STATE
-bool LEDSTATE;
-
 // CoAP server endpoint URL
-void callback_light(CoapPacket &packet, IPAddress ip, int port) {
-  Serial.println("[Light] ON/OFF");
+void callback_RTS(CoapPacket &packet, IPAddress ip, int port) {
+  Serial.println("RTS");
   
   // send response
   char p[packet.payloadlen + 1];
@@ -37,18 +27,27 @@ void callback_light(CoapPacket &packet, IPAddress ip, int port) {
   
   String message(p);
 
-  if (message.equals("0"))
-    LEDSTATE = false;
-  else if(message.equals("1"))
-    LEDSTATE = true;
-      
-  if (LEDSTATE) {
-    digitalWrite(LEDP, HIGH) ; 
-    coap.sendResponse(ip, port, packet.messageid, "1");
-  } else { 
-    digitalWrite(LEDP, LOW) ; 
-    coap.sendResponse(ip, port, packet.messageid, "0");
-  }
+  Serial.println(message);
+  Serial.println(ip);
+  Serial.println(port);   
+
+  coap.sendResponse(ip, port, packet.messageid, "0");
+}
+
+void callback_DATA(CoapPacket &packet, IPAddress ip, int port) {
+  Serial.println("RTS");
+  
+  // send response
+  char p[packet.payloadlen + 1];
+  memcpy(p, packet.payload, packet.payloadlen);
+  p[packet.payloadlen] = NULL;
+  
+  String message(p);
+
+  Serial.println(message);
+  Serial.println(ip);
+
+  coap.sendResponse(ip, port, packet.messageid, "0");
 }
 
 // CoAP client response callback
@@ -61,42 +60,31 @@ void callback_response(CoapPacket &packet, IPAddress ip, int port) {
   
   Serial.println(p);
 }
-void coap_loop(void *args) {
-    coap.put(IPAddress(192, 168, 20, 163), 5683, "light", "1");
-    while(1){
-        // send GET or PUT coap request to CoAP server.
-        // To test, use libcoap, microcoap server...etc
-        
-        Serial.println("Send Request");
-        int msgid = coap.get(IPAddress(192, 168, 20, 163), 5683, "light");
-        printf("%i\n",msgid);
 
-        delay(1000);
-        coap.loop();
-        }
+void coap_loop(void *args) {
+    coap.put(IPAddress(192, 168, 20, 163), 5683, "RTS", "asdf");
+    while(1){   
+    
+    Serial.println("Pidendo datos");
+    coap.get(IPAddress(192, 168, 20, 163), 5683, "RTS");
+
+    delay(1000);
+    coap.loop();
     }
+}
+
+void coap_stop(){
+    coap.
+}
 
 
 void coap_start() {
 
-  coap.server(callback_light, "light");
-
-  // client response callback.
-  // this endpoint is single callback.
-  Serial.println("Setup Response Callback");
-  coap.response(callback_response);
-
-  // start coap server/client
-  coap.start();
+  if(ChargingGroup.Params.GroupMaster){
+      coap.server(callback_RTS, "RTS");
+      coap.server(callback_DATA, "Data");
+  }
   
-  xTaskCreate(coap_loop,"coap", 4096,NULL,2,NULL);
-
-}
-
-void coap_client() {
-  
-  coap.server(callback_light, "light");
-
   // client response callback.
   // this endpoint is single callback.
   Serial.println("Setup Response Callback");
