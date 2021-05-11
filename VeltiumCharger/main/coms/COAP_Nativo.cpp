6e663ea91b2b41e0ac9613f7229b67750f32ad0b
Joel <joelmartinez@veltium.com>
2021-05-11 14:15:58 +0200

    client

--- VeltiumCharger/main/coms/COAP_Nativo.cpp
+++ VeltiumCharger/main/coms/COAP_Nativo.cpp
@@ -51,7 +51,7 @@ StaticTask_t xClientBuffer;
    (potentially associated with the IDENTITY)
 */
 #define EXAMPLE_COAP_PSK_KEY "CONFIG_EXAMPLE_COAP_PSK_KEY"
-
+#define COAP_DEFAULT_DEMO_URI "coaps://192.168.20.163/PARAMS"
 const static char *TAG = "CoAP_server";
 
 static char espressif_data[100];
@@ -172,16 +172,16 @@ static void hnd_espressif_put(coap_context_t *ctx,coap_resource_t *resource,coap
 }
 
 static void coap_client(void *p){
+   struct hostent *hp;
     coap_address_t    dst_addr;
     static coap_uri_t uri;
-    char server_uri[100];
-
-    sprintf(server_uri, "coaps://%s/PARAMS", ChargingGroup.MasterIP.toString().c_str());
+    const char       *server_uri = COAP_DEFAULT_DEMO_URI;
+    char *phostname = NULL;
 
     coap_set_log_level(LOG_DEBUG);
 
     while (1) {
-        #define BUFSIZE 40
+#define BUFSIZE 40
         unsigned char _buf[BUFSIZE];
         unsigned char *buf;
         size_t buflen;
@@ -200,12 +200,48 @@ static void coap_client(void *p){
             ESP_LOGE(TAG, "MbedTLS (D)TLS Client Mode not configured");
             break;
         }
+        if (uri.scheme == COAP_URI_SCHEME_COAPS_TCP && !coap_tls_is_supported()) {
+            ESP_LOGE(TAG, "CoAP server uri coaps+tcp:// scheme is not supported");
+            break;
+        }
 
-       
+        phostname = (char *)calloc(1, uri.host.length + 1);
+        if (phostname == NULL) {
+            ESP_LOGE(TAG, "calloc failed");
+            break;
+        }
+
+        memcpy(phostname, uri.host.s, uri.host.length);
+        hp = gethostbyname(phostname);
+        free(phostname);
+
+        if (hp == NULL) {
+            ESP_LOGE(TAG, "DNS lookup failed");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            free(phostname);
+            continue;
+        }
+        char tmpbuf[INET6_ADDRSTRLEN];
         coap_address_init(&dst_addr);
-        dst_addr.addr.sin.sin_family      = AF_INET;
-        dst_addr.addr.sin.sin_port        = htons(uri.port);
-        inet_aton(ChargingGroup.MasterIP.toString().c_str(),&dst_addr.addr.sin.sin_addr);
+        switch (hp->h_addrtype) {
+            case AF_INET:
+                dst_addr.addr.sin.sin_family      = AF_INET;
+                dst_addr.addr.sin.sin_port        = htons(uri.port);
+                memcpy(&dst_addr.addr.sin.sin_addr, hp->h_addr, sizeof(dst_addr.addr.sin.sin_addr));
+                inet_ntop(AF_INET, &dst_addr.addr.sin.sin_addr, tmpbuf, sizeof(tmpbuf));
+                ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", tmpbuf);
+                break;
+            case AF_INET6:
+                dst_addr.addr.sin6.sin6_family      = AF_INET6;
+                dst_addr.addr.sin6.sin6_port        = htons(uri.port);
+                memcpy(&dst_addr.addr.sin6.sin6_addr, hp->h_addr, sizeof(dst_addr.addr.sin6.sin6_addr));
+                inet_ntop(AF_INET6, &dst_addr.addr.sin6.sin6_addr, tmpbuf, sizeof(tmpbuf));
+                ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", tmpbuf);
+                break;
+            default:
+                ESP_LOGE(TAG, "DNS lookup response failed");
+                goto clean_up;
+        }
 
         if (uri.path.length) {
             buflen = BUFSIZE;
@@ -213,7 +249,10 @@ static void coap_client(void *p){
             res = coap_split_path(uri.path.s, uri.path.length, buf, &buflen);
 
             while (res--) {
-                coap_insert_optlist(&optlist, coap_new_optlist(COAP_OPTION_URI_PATH,coap_opt_length(buf),coap_opt_value(buf)));
+                coap_insert_optlist(&optlist,
+                                    coap_new_optlist(COAP_OPTION_URI_PATH,
+                                                     coap_opt_length(buf),
+                                                     coap_opt_value(buf)));
 
                 buf += coap_opt_size(buf);
             }
@@ -225,7 +264,11 @@ static void coap_client(void *p){
             res = coap_split_query(uri.query.s, uri.query.length, buf, &buflen);
 
             while (res--) {
-                coap_insert_optlist(&optlist, coap_new_optlist(COAP_OPTION_URI_QUERY, coap_opt_length(buf), coap_opt_value(buf)));
+                coap_insert_optlist(&optlist,
+                                    coap_new_optlist(COAP_OPTION_URI_QUERY,
+                                                     coap_opt_length(buf),
+                                                     coap_opt_value(buf)));
+
                 buf += coap_opt_size(buf);
             }
         }
@@ -236,12 +279,85 @@ static void coap_client(void *p){
             goto clean_up;
         }
 
-        if (uri.scheme == COAP_URI_SCHEME_COAPS) {
-            session = coap_new_client_session_psk(ctx, NULL, &dst_addr,COAP_PROTO_DTLS,ConfigFirebase.Device_Id,(const uint8_t *)EXAMPLE_COAP_PSK_KEY,sizeof(EXAMPLE_COAP_PSK_KEY) - 1);
+        /*
+         * Note that if the URI starts with just coap:// (not coaps://) the
+         * session will still be plain text.
+         *
+         * coaps+tcp:// is NOT supported by the libcoap->mbedtls interface
+         * so COAP_URI_SCHEME_COAPS_TCP will have failed in a test above,
+         * but the code is left in for completeness.
+         */
+        if (uri.scheme == COAP_URI_SCHEME_COAPS || uri.scheme == COAP_URI_SCHEME_COAPS_TCP) {
+#ifndef CONFIG_MBEDTLS_TLS_CLIENT
+            ESP_LOGE(TAG, "MbedTLS (D)TLS Client Mode not configured");
+            goto clean_up;
+#endif /* CONFIG_MBEDTLS_TLS_CLIENT */
+#ifdef CONFIG_COAP_MBEDTLS_PSK
+            session = coap_new_client_session_psk(ctx, NULL, &dst_addr,
+                                                  uri.scheme == COAP_URI_SCHEME_COAPS ? COAP_PROTO_DTLS : COAP_PROTO_TLS,
+                                                  ConfigFirebase.Device_Id,
+                                                  (const uint8_t *)EXAMPLE_COAP_PSK_KEY,
+                                                  sizeof(EXAMPLE_COAP_PSK_KEY) - 1);
+#endif /* CONFIG_COAP_MBEDTLS_PSK */
 
-        } 
-        else{
-            session = coap_new_client_session(ctx, NULL, &dst_addr, COAP_PROTO_UDP );
+#ifdef CONFIG_COAP_MBEDTLS_PKI
+            unsigned int ca_pem_bytes = ca_pem_end - ca_pem_start;
+            unsigned int client_crt_bytes = client_crt_end - client_crt_start;
+            unsigned int client_key_bytes = client_key_end - client_key_start;
+            coap_dtls_pki_t dtls_pki;
+            static char client_sni[256];
+
+            memset (&dtls_pki, 0, sizeof(dtls_pki));
+            dtls_pki.version = COAP_DTLS_PKI_SETUP_VERSION;
+            if (ca_pem_bytes) {
+                /*
+                 * Add in additional certificate checking.
+                 * This list of enabled can be tuned for the specific
+                 * requirements - see 'man coap_encryption'.
+                 *
+                 * Note: A list of root ca file can be setup separately using
+                 * coap_context_set_pki_root_cas(), but the below is used to
+                 * define what checking actually takes place.
+                 */
+                dtls_pki.verify_peer_cert        = 1;
+                dtls_pki.require_peer_cert       = 1;
+                dtls_pki.allow_self_signed       = 1;
+                dtls_pki.allow_expired_certs     = 1;
+                dtls_pki.cert_chain_validation   = 1;
+                dtls_pki.cert_chain_verify_depth = 2;
+                dtls_pki.check_cert_revocation   = 1;
+                dtls_pki.allow_no_crl            = 1;
+                dtls_pki.allow_expired_crl       = 1;
+                dtls_pki.allow_bad_md_hash       = 1;
+                dtls_pki.allow_short_rsa_length  = 1;
+                dtls_pki.validate_cn_call_back   = verify_cn_callback;
+                dtls_pki.cn_call_back_arg        = NULL;
+                dtls_pki.validate_sni_call_back  = NULL;
+                dtls_pki.sni_call_back_arg       = NULL;
+                memset(client_sni, 0, sizeof(client_sni));
+                if (uri.host.length) {
+                    memcpy(client_sni, uri.host.s, MIN(uri.host.length, sizeof(client_sni)));
+                } else {
+                    memcpy(client_sni, "localhost", 9);
+                }
+                dtls_pki.client_sni = client_sni;
+            }
+            dtls_pki.pki_key.key_type = COAP_PKI_KEY_PEM_BUF;
+            dtls_pki.pki_key.key.pem_buf.public_cert = client_crt_start;
+            dtls_pki.pki_key.key.pem_buf.public_cert_len = client_crt_bytes;
+            dtls_pki.pki_key.key.pem_buf.private_key = client_key_start;
+            dtls_pki.pki_key.key.pem_buf.private_key_len = client_key_bytes;
+            dtls_pki.pki_key.key.pem_buf.ca_cert = ca_pem_start;
+            dtls_pki.pki_key.key.pem_buf.ca_cert_len = ca_pem_bytes;
+
+            session = coap_new_client_session_pki(ctx, NULL, &dst_addr,
+                                                  uri.scheme == COAP_URI_SCHEME_COAPS ? COAP_PROTO_DTLS : COAP_PROTO_TLS,
+                                                  &dtls_pki);
+#endif /* CONFIG_COAP_MBEDTLS_PKI */
+        } else {
+            session = coap_new_client_session(ctx, NULL, &dst_addr,
+                                              uri.scheme == COAP_URI_SCHEME_COAP_TCP ? COAP_PROTO_TCP :
+                                              COAP_PROTO_UDP);
         }
         if (!session) {
             ESP_LOGE(TAG, "coap_new_client_session() failed");
@@ -263,7 +379,7 @@ static void coap_client(void *p){
         resp_wait = 1;
         coap_send(session, request);
 
-        wait_ms = 1 * 1000;
+        wait_ms = 2 * 1000;
 
         while (resp_wait) {
             int result = coap_run_once(ctx, wait_ms > 1000 ? 1000 : wait_ms);
@@ -292,12 +408,11 @@ clean_up:
          * change the following line to something like sleep(2)
          * if you want the request to continually be sent
          */
-        delay(1000);
+        break;
     }
 
     vTaskDelete(NULL);
 }
-
 
 
 static void coap_server(void *p){
