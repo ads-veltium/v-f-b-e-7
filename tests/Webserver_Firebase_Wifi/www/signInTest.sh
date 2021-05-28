c7550712453dbe50805cbd795cdbd05d1bdaab67
Joel <joelmartinez@veltium.com>
2021-05-28 12:37:30 +0200

    Pio eliminado

--- tests/Webserver_Firebase_Wifi/www/signInTest.sh
+++
@@ -1,3 +0,0 @@
-curl 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBaJA88_Y3ViCzNF_J08f4LBMAM771aZLs' \
--H 'Content-Type: application/json' \
---data-binary '{"email":"grantasca@yahoo.es","password":"pepepe","returnSecureToken":true}'
