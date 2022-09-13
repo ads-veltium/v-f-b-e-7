a5f058ddfe2fca34df1ad7f5dd1769a220e3363e
Jon Etxezarreta <jonetxezarreta@veltium.com>
2022-09-13 17:31:04 +0200

    VBLE2_0519

--- VeltiumCharger/main/control.cpp
+++ VeltiumCharger/main/control.cpp
@@ -92,7 +92,7 @@ uint16 cnt_diferencia = 1;
 uint8 HPT_estados[9][3] = {"0V", "A1", "A2", "B1", "B2", "C1", "C2", "E1", "F1"};
 
 #ifdef USE_COMS
-uint8 version_firmware[11] = {"VBLE2_0500"};	
+uint8 version_firmware[11] = {"VBLE2_0519"};	
 #else
 uint8 version_firmware[11] = {"VBLE0_0516"};	
 #endif
